d894c3698ee13abdc340761feda1a4cad154748a
Ismael <Ismael.Tortosa@hotmail.com>
2022-10-13 11:35:41 +0200

    corregit

--- S1T3N2E2/Main.java
+++ S1T3N2E2/Main.java
@@ -3,6 +3,9 @@ package S1T3N2E2;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.TreeSet;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Main {
 
@@ -19,12 +22,17 @@ public class Main {
 		restaurants.add(new Restaurant("El Programador", 10));
 		restaurants.add(new Restaurant("El Programador", 9));
 		
+		/*
 		//cambiem a una ArrayList per fer ordenar el restaurants
 		ArrayList<Restaurant> LlistaRestaurants = new ArrayList<Restaurant>(restaurants);
 		
 		//agafem de la classe Collections el mètode sort per endreçar
 		Collections.sort(LlistaRestaurants); 
 		//Collections.reverse(LlistaRestaurants); endreçar al reves Z a A
+		*/
+		
+		//classe TreeSet
+		TreeSet<Restaurant> LlistaRestaurants = new TreeSet<Restaurant>(restaurants);
 		
 		//mostrem la llista
 		for (Restaurant restaurant : LlistaRestaurants) {
